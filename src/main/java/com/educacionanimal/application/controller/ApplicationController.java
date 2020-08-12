e14227ae6e72acb8bece81623e332896b4bb0e26
Juanma <jmlpdr.93@gmail.com>
2020-08-12 18:40:17 +0100

    :tada: first commit

---
+++ src/main/java/com/educacionanimal/application/controller/ApplicationController.java
@@ -0,0 +1,49 @@
+package com.educacionanimal.application.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @RequestMapping("/presentation")
+    public String displayPresentation() {
+        return "Hola mundo presentacion";
+    }
+
+    @RequestMapping("/virtual-visit")
+    public String displayVirtualVisit() {
+        return "Hola mundo visita virtual";
+    }
+
+    @RequestMapping("/volunteering")
+    public String displayVolunteering() {
+        return "Hola mundo voluntario";
+    }
+
+    @RequestMapping("/responsible-care")
+    public String displayResponsibleCare() {
+        return "Hola mundo responsable";
+    }
+
+    @RequestMapping("/animal-protection")
+    public String displayAnimalProtection() {
+        return "Hola mundo protección";
+    }
+
+    @RequestMapping("/adoption")
+    public String displayAdoption() {
+        return "Hola mundo adopción";
+    }
+
+    @RequestMapping("/euthanasia")
+    public String displayEuthanasia() {
+        return "Hola mundo eutanasia";
+    }
+
+    @RequestMapping("/game")
+    public String displayGame() {
+        return "Hola mundo juego";
+    }
+}
