b1f0ad4d0e6ee4fe29737aef772ea3548e61860c
Juan Manuel <59283605+juanmaMacGyverCode@users.noreply.github.com>
2020-08-13 11:38:14 +0100

    Update ApplicationController.java

--- src/main/java/com/educacionanimal/application/controller/ApplicationController.java
+++ src/main/java/com/educacionanimal/application/controller/ApplicationController.java
@@ -9,7 +9,7 @@ public class ApplicationController {
 
     @RequestMapping("/presentation")
     public String displayPresentation() {
-        return "Hola mundo presentacion MODIFICADO";
+        return "Hola mundo presentacion MODIFICADO HOLA HOLA HOLA";
     }
 
     @RequestMapping("/virtual-visit")
